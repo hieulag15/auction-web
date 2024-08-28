1de2944143fd872dcb39e78294a3e75ac6c13320
hieulag15 <hieuthanhtran12@gmail.com>
2024-08-28 14:28:43 +0700

    init

---
+++ backend/src/main/java/com/example/auction_web/AuctionWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.auction_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionWebApplication.class, args);
+	}
+
+}
