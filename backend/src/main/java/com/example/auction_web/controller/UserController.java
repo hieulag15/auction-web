922f3b2e571b4cdc1d5a69ac450faf82e92ac3bf
BaoNun1205 <94816323+BaoNun1205@users.noreply.github.com>
2024-08-29 22:27:31 +0700

    test create user

---
+++ backend/src/main/java/com/example/auction_web/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.auction_web.controller;
+
+import com.example.auction_web.dto.request.UserCreateRequest;
+import com.example.auction_web.dto.response.ApiResponse;
+import com.example.auction_web.dto.response.UserResponse;
+import com.example.auction_web.entity.User;
+import com.example.auction_web.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserController {
+    UserService userService;
+
+    @PostMapping
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreateRequest request){
+        return ApiResponse.<User>builder()
+                .result(userService.createUser(request))
+                .build();
+    }
+}
