10344fbb4fa4596a5221d001efec62d784698720
DongDz191 <pdongdz191@gmail.com>
2024-09-27 19:55:11 +0700

    form create asset

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -3,6 +3,7 @@ import HomePage from '~/pages/Home/HomePage'
 import Authentication from '~/pages/Authentication/Authentication'
 import User from '~/pages/Customer/User'
 import ConfirmAccount from '~/features/Authentication/comfirm'
+import AddAssetPage from '~/pages/Asset/AddAssetPage'
 
 const BASE_PATHS = {
   HOME: '/',
@@ -22,6 +23,11 @@ export const routes = [
     path: `${BASE_PATHS.ASSET}/list`,
     page: AssetListPage
   },
+  {
+    path: `${BASE_PATHS.ASSET}/create`,
+    page: AddAssetPage
+  },
+
   {
     path: BASE_PATHS.HOME,
     page: HomePage
