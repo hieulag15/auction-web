cf2f0b5c8648705c531d29bb9d700f93ea7f5dda
BaoNun1205 <94816323+BaoNun1205@users.noreply.github.com>
2024-10-28 14:27:51 +0700

    fix conflig

--- backend/src/main/java/com/example/auction_web/repository/RequirementRepository.java
+++ backend/src/main/java/com/example/auction_web/repository/RequirementRepository.java
@@ -1,6 +1,9 @@
 package com.example.auction_web.repository;
 
 import com.example.auction_web.entity.Requirement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface RequirementRepository extends JpaRepository<Requirement, String> {
-    List<Requirement> findRequirementsByUser_UserId(String userId);
-    List<Requirement> findRequirementsByInsprector_InsprectorId(String insprectorId);
+    List<Requirement> findRequirementsByVendor_UserId(String userId);
+    List<Requirement> findRequirementsByInspector_UserId(String userId);
+    Page<Requirement> findAll(Specification<Requirement> specification, Pageable pageable);
 }
