7a136ace09594c25aaeb15384e1803985a30437d
HieuTT <hieuthanhtran12@gmail.com>
2024-10-30 12:41:48 +0700

    total

--- backend/src/main/java/com/example/auction_web/service/CategoryService.java
+++ backend/src/main/java/com/example/auction_web/service/CategoryService.java
@@ -10,6 +10,7 @@ import java.util.List;
 //@FieldDefaults(level = lombok.AccessLevel.PUBLIC)
 public interface CategoryService {
     List<CategoryResponse> filterCategories(Boolean status, String keyword, int page, int size);
+    int totalCategories(Boolean status, String keyword);
     CategoryResponse getCategory(String id);
     CategoryResponse createCategory(CategoryCreateRequest request);
     CategoryResponse updateCategory(String id, CategoryUpdateRequest request);
