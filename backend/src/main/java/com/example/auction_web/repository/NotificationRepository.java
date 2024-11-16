f28c674133f78f30ed19ff0b2a7e4b8dbc9a1674
HieuTT <hieuthanhtran12@gmail.com>
2024-11-16 11:07:45 +0700

    Reschule notification register

---
+++ backend/src/main/java/com/example/auction_web/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.example.auction_web.repository;
+
+import com.example.auction_web.entity.NotificationLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<NotificationLog, String> {
+    List<NotificationLog> findByStatus(NotificationLog.NotificationStatus status);
+    NotificationLog findNotificationLogByAuctionSessionIdAndEmail(String auctionSessionId, String email);
+}
